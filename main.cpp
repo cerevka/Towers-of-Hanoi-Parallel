8d7271b77d8be8d25754d4825b8ac61b52f51e8d
Tomáš Čerevka <tomas.cerevka@gmail.com>
2011-10-31 15:58:46 +0100

    Inicialization.

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Tomáš Čerevka
+ *
+ * Created on October 31, 2011, 3:21 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
